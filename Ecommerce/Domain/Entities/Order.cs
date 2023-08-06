843aebaea45679a6f2917eec295dbc27b8762e07
ruhtar <arthur.amorim10@gmail.com>
2023-08-05 22:54:09 -0300

    Project rename

---
+++ Ecommerce/Domain/Entities/Order.cs
@@ -0,0 +1,28 @@
+﻿using AuthenticationService.Domain.Aggregates;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AuthenticationService.Domain.Entities
+{
+    public class Order
+    {
+        [Key]
+        [Column("Id")]
+        public int OrderId { get; set; }
+        public int Quantity { get; set; }
+        public int ShoppingCartId { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+        [JsonIgnore]
+        public ICollection<OrderProduct> OrderProducts { get; set; }
+    }
+
+    public class OrderProduct
+    {
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+    }
+}
