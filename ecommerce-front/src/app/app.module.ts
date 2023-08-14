3f518496fbfaf7f985836291139de55c5fea9f65
ruhtar <arthur.amorim10@gmail.com>
2023-08-14 19:32:24 -0300

    front things

--- ecommerce-front/src/app/app.module.ts
+++ ecommerce-front/src/app/app.module.ts
@@ -7,9 +7,13 @@ import { LoginComponent } from './components/login/login.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LoginService } from './services/login/login.service';
 import { HttpClientModule } from '@angular/common/http';
-import { HeaderComponent } from './components/home/header/header.component';
-import { FooterComponent } from './components/home/footer/footer.component';
-import { MainPageComponent } from './components/home/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { HomeComponent } from './components/main-page/home/home.component';
+import { ProductsComponent } from './components/main-page/products/products.component';
+import { CartComponent } from './components/main-page/cart/cart.component';
+import { ContactComponent } from './components/main-page/contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +21,11 @@ import { MainPageComponent } from './components/home/main-page/main-page.compone
     LoginComponent,
     HeaderComponent,
     FooterComponent,
-    MainPageComponent
+    MainPageComponent,
+    HomeComponent,
+    ProductsComponent,
+    CartComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
