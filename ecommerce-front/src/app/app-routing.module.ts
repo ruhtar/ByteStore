38cd069e0f0a034b5b523862a3e61315ce8f38c1
ruhtar <arthur.amorim10@gmail.com>
2023-08-14 19:48:57 -0300

    routes

--- ecommerce-front/src/app/app-routing.module.ts
+++ ecommerce-front/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
-import { MainPageComponent } from './components/main-page/main-page.component';
+import { ProductsComponent } from './components/main-page/products/products.component';
+import { HomeComponent } from './components/main-page/home/home.component';
+import { CartComponent } from './components/main-page/cart/cart.component';
+import { ContactComponent } from './components/main-page/contact/contact.component';
 
 const routes: Routes = [
   {path: "login", component: LoginComponent},
-  {path: "", component: MainPageComponent},
+  {path: "home", component: HomeComponent},
+  {path: "", component: HomeComponent},
+  {path: "products", component: ProductsComponent},
+  {path: "cart", component: CartComponent},
+  {path: "contact", component: ContactComponent},
 ];
 
 @NgModule({
