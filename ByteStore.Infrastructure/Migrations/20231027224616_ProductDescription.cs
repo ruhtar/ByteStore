4cd306c5925c6fcf255bfde2ef77556ac1dd3294
ruhtar <arthur.amorim10@gmail.com>
2023-10-27 20:29:49 -0300

    migration and css

---
+++ ByteStore.Infrastructure/Migrations/20231027224616_ProductDescription.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ByteStore.Infrastructure.Migrations
+{
+    public partial class ProductDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Products");
+        }
+    }
+}
