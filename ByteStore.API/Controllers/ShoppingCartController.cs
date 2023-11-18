ad61c5621d0e3c82d592ea8e8e81a73c9799197c
ruhtar <arthur.amorim10@gmail.com>
2023-11-17 22:40:35 -0300

    cleanup comments

--- ByteStore.API/Controllers/ShoppingCartController.cs
+++ ByteStore.API/Controllers/ShoppingCartController.cs
@@ -5,7 +5,6 @@ using ByteStore.Shared.DTO;
 using ByteStore.Shared.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace ByteStore.API.Controllers;
 
@@ -16,7 +15,7 @@ namespace ByteStore.API.Controllers;
 public class ShoppingCartController : ControllerBase
 {
     private readonly IShoppingCartService _shoppingCartService;
-
+    
     public ShoppingCartController(IShoppingCartService shoppingCartService)
     {
         _shoppingCartService = shoppingCartService;
@@ -25,17 +24,14 @@ public class ShoppingCartController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<OrderStatus>> MakeOrder(OrderItem item, int userId)
     {
-        // if (!IsTokenValid()) return Unauthorized();
         var result = await _shoppingCartService.MakeOrder(item, userId);
         if (result == OrderStatus.Approved) return Ok(result.ToString());
-        if (result == OrderStatus.InvalidQuantity) return BadRequest(result.ToString());
         return BadRequest(result.ToString());
     }
 
     [HttpGet]
     public async Task<ActionResult> BuyOrder(int userId)
     {
-        // if (!IsTokenValid()) return Unauthorized();
         var result = await _shoppingCartService.BuyOrder(userId);
         if (result == BuyOrderStatus.InvalidQuantity) return BadRequest(result.ToString());
         return Ok(BuyOrderStatus.Completed.ToString());
@@ -45,16 +41,13 @@ public class ShoppingCartController : ControllerBase
     public async Task<ActionResult<ShoppingCartResponseDto>> GetShoppingCartByUserAggregateId(
         [FromRoute] int userAggregateId)
     {
-        // if (!IsTokenValid()) return Unauthorized();
         var cart = await _shoppingCartService.GetShoppingCartByUserAggregateId(userAggregateId);
-        if (cart == null) return Problem("The shopping cart of the current user is not available.");
-        return Ok(cart);
+        return cart == null ? Problem("The shopping cart of the current user is not available.") : Ok(cart);
     }
 
     [HttpDelete("users/{userAggregateId}")]
     public async Task<IActionResult> RemoveProductFromShoppingCart(int userAggregateId, int productId)
     {
-        // if (!IsTokenValid()) return Unauthorized();
         await _shoppingCartService.RemoveProductFromCart(userAggregateId, productId);
         return Ok();
     }
--- ByteStore.Application/Services/Interfaces/IShoppingCartService.cs
+++ ByteStore.Application/Services/Interfaces/IShoppingCartService.cs
@@ -7,8 +7,6 @@ namespace ByteStore.Application.Services.Interfaces;
 public interface IShoppingCartService
 {
     Task<BuyOrderStatus> BuyOrder(int userAggregateId);
-
-    //Task<ShoppingCartDto?> GetShoppingCartById(int shoppingCartId);
     Task<ShoppingCartResponseDto?> GetShoppingCartByUserAggregateId(int userAggregateId);
     Task<OrderStatus?> MakeOrder(OrderItem itemToAdd, int userAggregateId);
     Task RemoveProductFromCart(int userAggregateId, int productId);
