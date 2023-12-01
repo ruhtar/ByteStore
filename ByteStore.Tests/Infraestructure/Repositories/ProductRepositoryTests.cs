699b7b8740aa90772dfafcc42e0a23f5e1c8d9bd
ruhtar <arthur.amorim10@gmail.com>
2023-12-01 20:53:24 -0300

    GetAllProducts_ReturnsCorrectProducts

--- ByteStore.Tests/Infraestructure/Repositories/ProductRepositoryTests.cs
+++ ByteStore.Tests/Infraestructure/Repositories/ProductRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using ByteStore.Domain.Entities;
+using ByteStore.Domain.ValueObjects;
 using ByteStore.Infrastructure.Repositories;
 using ByteStore.Shared.DTO;
 using Xunit;
@@ -14,13 +15,18 @@ public class ProductRepositoryTests
         var productRepository = Utils.GetProductRepository();
 
         // Act
-        var input = new GetProductsInputPagination { PageSize = 10, PageIndex = 0 };
+        await productRepository.AddProduct(Utils.GetProductMocks()[0]); 
+        await productRepository.AddProduct(Utils.GetProductMocks()[1]);
+        
+        var input = new GetProductsInputPagination { PageSize = 2, PageIndex = 0 };
         var result = await productRepository.GetAllProducts(input);
-
+            
         // Assert
         Assert.NotNull(result);
         Assert.IsType<List<Product>>(result);
         Assert.Equal(input.PageSize, result.Count);
+        Assert.Equivalent(Utils.GetProductMocks()[0], result[0]);
+        Assert.Equivalent(Utils.GetProductMocks()[1], result[1]);
     }
     
     [Fact]
@@ -30,11 +36,111 @@ public class ProductRepositoryTests
         var productRepository = Utils.GetProductRepository();
 
         // Act
-        var result = await productRepository.AddProduct(Utils.GetProductMock());
+        var result = await productRepository.AddProduct(Utils.GetProductMocks()[0]);
 
         // Assert
         Assert.NotNull(result);
         Assert.IsType<Product>(result);
-        Assert.Equal(Utils.GetProductMock().ProductId, result.ProductId);
+        Assert.Equal(Utils.GetProductMocks()[0].ProductId, result.ProductId);
+    }
+    
+    [Fact]
+    public async Task GetProductById_ReturnsCorrectProduct()
+    {
+        // Arrange
+        var dbContext = Utils.GetDbContext();
+        var productRepository = new ProductRepository(dbContext);
+
+        // Add a product for testing
+        var newProduct = new Product { /* Set product properties */ };
+        await productRepository.AddProduct(newProduct);
+
+        // Act
+        var result = await productRepository.GetProductById(newProduct.ProductId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Product>(result);
+        Assert.Equal(newProduct.ProductId, result.ProductId);
+    }
+    
+    [Fact]
+    public async Task UpdateProduct_ReturnsTrueOnSuccess()
+    {
+        // Arrange
+        var dbContext = Utils.GetDbContext();
+        var productRepository = new ProductRepository(dbContext);
+
+        // Add a product for testing
+        var newProduct = new Product { /* Set product properties */ };
+        await productRepository.AddProduct(newProduct);
+
+        // Act
+        var updateDto = new UpdateProductDto { /* Set updated properties */ };
+        var result = await productRepository.UpdateProduct(newProduct.ProductId, updateDto);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task DeleteProduct_ReturnsTrueOnSuccess()
+    {
+        // Arrange
+        var dbContext = Utils.GetDbContext();
+        var productRepository = new ProductRepository(dbContext);
+
+        // Add a product for testing
+        var newProduct = new Product { /* Set product properties */ };
+        await productRepository.AddProduct(newProduct);
+
+        // Act
+        var result = await productRepository.DeleteProduct(newProduct.ProductId);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public async Task GetReviews_ReturnsReviewsForProduct()
+    {
+        // Arrange
+        var dbContext = Utils.GetDbContext();
+        var productRepository = new ProductRepository(dbContext);
+
+        // Add a product with reviews for testing
+        var newProduct = new Product { /* Set product properties */ };
+        var newReview = new Review { /* Set review properties */ };
+        newProduct.Reviews = new List<Review> { newReview };
+        await productRepository.AddProduct(newProduct);
+
+        // Act
+        var result = await productRepository.GetReviews(newProduct.ProductId);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<List<Review>>(result);
+        Assert.Equal(newReview.Id, result.FirstOrDefault()?.Id);
+    }
+
+    [Fact]
+    public async Task CreateReview_ReturnsAddedReview()
+    {
+        // Arrange
+        var dbContext = Utils.GetDbContext();
+        var productRepository = new ProductRepository(dbContext);
+
+        // Add a product for testing
+        var newProduct = new Product { /* Set product properties */ };
+        await productRepository.AddProduct(newProduct);
+
+        // Act
+        var newReview = new Review { /* Set review properties */ };
+        var result = await productRepository.CreateReview(newReview);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<Review>(result);
+        Assert.Equal(newReview.Id, result.Id);
     }
 }
--- ByteStore.Tests/Utils.cs
+++ ByteStore.Tests/Utils.cs
@@ -21,9 +21,11 @@ public abstract class Utils
         return new ProductRepository(dbContext);
     }
 
-    internal static Product GetProductMock()
+internal static List<Product> GetProductMocks()
+{
+    return new List<Product>
     {
-        return new Product
+        new Product
         {
             ProductId = 1,
             ProductQuantity = 10,
@@ -34,6 +36,68 @@ public abstract class Utils
             Reviews = null,
             Rate = 5,
             TimesRated = 777
-        };
-    }
+        },
+        new Product
+        {
+            ProductId = 2,
+            ProductQuantity = 20,
+            Name = "RAQUETE DE AR",
+            Price = new decimal(149.99),
+            ImageStorageUrl = "URL_RAQUETE",
+            Description = "PERFEITA PARA JOGAR EM AMBIENTES SEM GRAVIDADE",
+            Reviews = null,
+            Rate = 4,
+            TimesRated = 543
+        },
+        new Product
+        {
+            ProductId = 3,
+            ProductQuantity = 15,
+            Name = "MEIAS ANTI-GRAVIDADE",
+            Price = new decimal(29.99),
+            ImageStorageUrl = "URL_MEIAS",
+            Description = "VOE COM ESTILO USANDO NOSSAS MEIAS REVOLUCIONÁRIAS",
+            Reviews = null,
+            Rate = 4.5,
+            TimesRated = 321
+        },
+        new Product
+        {
+            ProductId = 4,
+            ProductQuantity = 5,
+            Name = "CAPACETE DE NATAÇÃO COM VISÃO NOTURNA",
+            Price = new decimal(79.99),
+            ImageStorageUrl = "URL_CAPACETE",
+            Description = "NADA COM SEGURANÇA MESMO NA ESCURIDÃO TOTAL",
+            Reviews = null,
+            Rate = 3.8,
+            TimesRated = 432
+        },
+        new Product
+        {
+            ProductId = 5,
+            ProductQuantity = 25,
+            Name = "LUVA DE ESCREVER NO AR",
+            Price = new decimal(49.99),
+            ImageStorageUrl = "URL_LUVA",
+            Description = "CRIE PALAVRAS NO AR COM NOSSA LUVA MÁGICA",
+            Reviews = null,
+            Rate = 4.2,
+            TimesRated = 210
+        },
+        new Product
+        {
+            ProductId = 6,
+            ProductQuantity = 8,
+            Name = "CANETA AUTODESTRUTIVA",
+            Price = new decimal(19.99),
+            ImageStorageUrl = "URL_CANETA",
+            Description = "ESCREVA MENSAGENS SECRETAS QUE SE AUTODESTRUÍRÃO",
+            Reviews = null,
+            Rate = 3.9,
+            TimesRated = 298
+        },
+        // Adicione mais produtos conforme necessário
+    };
+}
 }
